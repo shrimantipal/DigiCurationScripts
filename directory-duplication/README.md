175a757e55f4fd74be37784e66595f01ed74468e
Shri <59371984+shrimantipal@users.noreply.github.com>
2024-04-10 14:00:58 -0500

    directory duplication edits

--- directory-duplication/README.md
+++ directory-duplication/README.md
@@ -3,17 +3,11 @@
 ### Example: the subfolder "preservation" contains multiple subfolders that are named after barcode numbers, all of which contain subfolders as well. The objective is to copy all the barcode subfolders in a subfolder called "nearline" of the folder "access" which is at the same level as the "preservation" folder, without copying the contents of the barcode subfolders.
 
 
-The variable start_path contains the path of the folder where the directory contents need to be duplicated:
+The variable start_path contains the path of the folder whose directory contents need to be duplicated. 
 
 ```python
-start_path = r'C:\Users\pal10\Desktop\GAPSFiles\GAPSTestsetCopy\access\nearline'
+start_path = r'C:/Users/pal10/Desktop/GAPSFiles/GAPSTestsetCopy/preservation'
 ```
 
-Change this variable to whichever path you want to copy the structure of the duplicate variable.
-
-The variable duplicate contains the path of the folder whose directory contents need to be duplicated. Change this accordingly as well:
-
-```python
-duplicate = r'C:\Users\pal10\Desktop\GAPSFiles\GAPSTestsetCopy\preservation'
-```
+Change this variable to whichever path you want to copy the structure of the duplicate variable. Running the script will create a duplicate directory structure of the preservation folder in the access/nearline folder.
 
--- directory-duplication/directory_structure.py
+++ directory-duplication/directory_structure.py
@@ -1,26 +1,26 @@
 import os
 
-def find_subfolder(start_path, dup):
-    # Check if the specified path exists
+def find_subfolder(dup):
+
     if not os.path.exists(start_path):
         print(f"Path {start_path} does not exist.")
         return []
 
-    root_contents = os.listdir(dup)
+    contents = os.listdir(dup)
 
-    for content in root_contents:
-        content_path = os.path.join(start_path, content)
-        # new_path = os.path.join(os.path.dirname(start_path), content)
-        print(content_path)
-        print(content)
-        os.makedirs(content_path, exist_ok=True)
-        # subfolder_names = ['OriginalFiles', 'ObjectDocumentation', 'ImageCapturedFromDisk']
-        # subfolder_paths = [os.path.join(content_path, subfolder_name) for subfolder_name in subfolder_names]
-        # for subfolder_path in subfolder_paths:
-        #     os.makedirs(subfolder_path, exist_ok=True)
-        #     print(f"Subfolder '{subfolder_path}' created.")
+    for content in contents:
+        
+        content_path = os.path.join(dup, content)
+        
+        if os.path.isdir(content_path):
+            print(f"Entering subfolder: {content_path}")
+            create = content_path.replace("preservation", "access/nearline")
+            os.makedirs(create, exist_ok=True)
+            find_subfolder(content_path)
+
+        else:
+            print(os.path.dirname(content_path))
 
 
 start_path = r'C:\Users\pal10\Desktop\GAPSFiles\GAPSTestsetCopy\access\nearline'
-duplicate = r'C:\Users\pal10\Desktop\GAPSFiles\GAPSTestsetCopy\preservation'
-find_subfolder(start_path, duplicate)
+find_subfolder(start_path)
